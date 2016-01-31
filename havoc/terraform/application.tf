577a4d8fd6fb331a7ddbc7e852e8e0b33b0192c0
Jacob Dearing <jacob.dearing@gmail.com>
2016-01-31 16:50:32 -0600

    update application comments and include useless userdata

--- havoc/terraform/application.tf
+++ havoc/terraform/application.tf
@@ -1,9 +1,17 @@
-# A security group for the ELB so it is accessible via the web
+
+/*
+  ======================================================
+    'SECURITY GROUPS'
+  ======================================================
+*/
+
+# security for the load balancer
 resource "aws_security_group" "elb" {
   name = "havoc-elb-sg"
   description = "HAVOC ELB SG"
   vpc_id      = "${aws_vpc.default.id}"
 
+  # world accesible on 443
   ingress {
     from_port   = 443
     to_port     = 443
@@ -23,11 +31,13 @@ resource "aws_security_group" "elb" {
   }
 }
 
+# security for the autoscaled instances
 resource "aws_security_group" "ec2" {
   name = "havoc-ec2-sg"
   description = "HAVOC EC2 SG"
   vpc_id      = "${aws_vpc.default.id}"
 
+  # anyone within the VPC can ssh in
   ingress {
     from_port   = 22
     to_port     = 22
@@ -35,6 +45,7 @@ resource "aws_security_group" "ec2" {
     cidr_blocks = ["${lookup(var.vpc_cidrs, "VPC")}"]
   }
 
+  # only the load-balancer needs to read port 8080 (our web app)
   ingress {
     from_port   = 8080
     to_port     = 8080
@@ -54,6 +65,11 @@ resource "aws_security_group" "ec2" {
   }
 }
 
+/*
+  ======================================================
+    'MAIN LOAD BALANCER'
+  ======================================================
+*/
 
 resource "aws_elb" "default" {
   subnets         = ["${aws_subnet.ext1.id}", "${aws_subnet.ext2.id}"]
@@ -63,11 +79,14 @@ resource "aws_elb" "default" {
   connection_draining = true
   connection_draining_timeout = 60
 
+
+  # forward 443 terminated HTTPS traffic to port 8080 on the instances
   listener {
     instance_port     = 8080
     instance_protocol = "http"
     lb_port           = 443
-    lb_protocol       = "https"  # *.racker.tech
+    lb_protocol       = "https"
+    # attach our ready `*.racker.tech` cert to this listener
     ssl_certificate_id = "arn:aws:acm:us-east-1:595430538023:certificate/5d611a4a-6348-4134-9ccd-4b92e25469ac"
   }
 
@@ -84,6 +103,13 @@ resource "aws_elb" "default" {
   }
 }
 
+/*
+  ======================================================
+    'AUTOSCALING AND LAUNCH CONFIG'
+  ======================================================
+*/
+
+# TODO: smooth out handling of extant keypair
 resource "aws_key_pair" "default" {
   key_name   = "${var.key_name}"
   public_key = "${file(var.public_key_path)}"
@@ -94,15 +120,21 @@ resource "aws_launch_configuration" "default" {
   image_id = "${lookup(var.aws_amis, var.aws_region)}"
   instance_type = "t2.small"
   security_groups = ["${aws_security_group.ec2.id}"]
-  # user_data = "${file("userdata.sh")}"
   key_name = "${var.key_name}"
   enable_monitoring = true
+
+  # this tells TERRAFORM to create a new launch_config before swapping it out at the ASG
+  # otherwise you get an error because you cannot delete what is in use.
   lifecycle {
     create_before_destroy = true
   }
+
+  # using PACKER to prepare AMI's haven't needed userdata yet here for clarity.
+  user_data = "${file("userdata.sh")}"
 }
 
 
+# launch instances in both subnets provided and place them into the load balancer
 resource "aws_autoscaling_group" "default" {
   vpc_zone_identifier = ["${aws_subnet.int1.id}","${aws_subnet.int2.id}"]
   max_size = 6
@@ -119,6 +151,13 @@ resource "aws_autoscaling_group" "default" {
   }
 }
 
+/*
+  ======================================================
+    'DNS MANAGEMENT'
+  ======================================================
+*/
+
+# create an alias record on the extant zone for this ELB
 resource "aws_route53_record" "default" {
   zone_id = "Z2EBPSP8GXVCP4"
   name = "havoc.racker.tech"
